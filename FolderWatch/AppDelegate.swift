09ad9f87193b41c4c3ee3121434aae3246158703
Oliver Michalak <oliver@werk01.de>
2020-04-03 10:22:00 +0200

    Initial Commit

---
+++ FolderWatch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FolderWatch
+//
+//  Created by Oliver Michalak on 03.04.20.
+//  Copyright © 2020 Oliver Michalak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ FolderWatch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FolderWatch
+//
+//  Created by Oliver Michalak on 03.04.20.
+//  Copyright © 2020 Oliver Michalak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
